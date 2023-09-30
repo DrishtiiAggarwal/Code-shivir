ded844f3c5020386b66c969384ff0359f4327d7d
Drishti Aggarwal <97733511+DrishtiiAggarwal@users.noreply.github.com>
2023-09-30 13:45:04 +0530

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Navbar from './components/Navbar'; // Adjust the path based on your project structure
 import HomePage from './components/HomePage'; // Adjust the path based on your project structure
-import AboutUsPage from './components/AboutUsPage'; // Import the AboutUsPage component
+//import AboutUsPage from './components/AboutUsPage'; // Import the AboutUsPage component
 import FaqsPage from './components/FaqsPage';
 import ContactUsPage from './components/ContactUsPage';
 import AboutMePage from './components/AboutMe';
@@ -14,7 +14,6 @@ const App: React.FC = () => {
     <div>
       <Navbar />
       <HomePage />
-      <AboutUsPage />
       <AboutMePage/>
       <FaqsPage />
       <ContactUsPage />
